a6da357689276cb1d95200667e7f9742e535651e
Reinhard <567396+suamor@users.noreply.github.com>
2026-05-25 20:36:36 +0200

    Boost CMake updates

--- CMake/Boost.cmake
+++ CMake/Boost.cmake
@@ -10,5 +10,4 @@ FetchContent_MakeAvailable(Boost)
 
 # Enable only needed Boost libraries
 set(BOOST_INCLUDE_LIBRARIES filesystem thread serialization date_time)
-#add_subdirectory(${boost_SOURCE_DIR} ${boost_BINARY_DIR} EXCLUDE_FROM_ALL)
 unset(CMAKE_POLICY_VERSION_MINIMUM)
