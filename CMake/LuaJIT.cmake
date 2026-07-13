79e75e4738410ecbae109b339449c85e5b6a2008
Reinhard <567396+suamor@users.noreply.github.com>
2026-07-13 22:23:17 +0200

    Fix Luajit.cmake handling of cmd

--- CMake/LuaJIT.cmake
+++ CMake/LuaJIT.cmake
@@ -31,8 +31,8 @@ ExternalProject_Add(
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/lauxlib.h      <INSTALL_DIR>/include/ &&
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.h       <INSTALL_DIR>/include/ &&
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luaconf.h      <INSTALL_DIR>/include/ &&
-        ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.lib     <INSTALL_DIR>/lib/
-        ${CMAKE_COMMAND} -E cmd /c "dir /s ${LUAJIT_PREFIX}"
+        ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.lib     <INSTALL_DIR>/lib/ &&
+        ${CMAKE_COMMAND} cmd /c "dir /s ${LUAJIT_PREFIX}"
 )
 
 ExternalProject_Get_Property(LuaJIT INSTALL_DIR)
