65478505f93eac93cb6255309fdd1779ff435b27
Reinhard <567396+suamor@users.noreply.github.com>
2026-07-13 22:11:08 +0200

    Go one level up in analysis. This CI is copying files and then they are gone?

--- CMake/LuaJIT.cmake
+++ CMake/LuaJIT.cmake
@@ -32,7 +32,7 @@ ExternalProject_Add(
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.h       <INSTALL_DIR>/include/ &&
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luaconf.h      <INSTALL_DIR>/include/ &&
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.lib     <INSTALL_DIR>/lib/
-        ${CMAKE_COMMAND} -E cmd /c "dir /s ${LUAJIT_INSTALL_DIR}"
+        ${CMAKE_COMMAND} -E cmd /c "dir /s ${LUAJIT_PREFIX}"
 )
 
 ExternalProject_Get_Property(LuaJIT INSTALL_DIR)
