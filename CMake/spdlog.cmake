da52ee1c45dfc627512f9773848040dcb92f9763
Reinhard <567396+suamor@users.noreply.github.com>
2026-07-15 10:35:49 +0200

    Downgraded spdlog to 10.0 to stop linker errors

--- CMake/spdlog.cmake
+++ CMake/spdlog.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     spdlog
     GIT_REPOSITORY https://github.com/gabime/spdlog.git
-    GIT_TAG v1.12.0
+    GIT_TAG v1.10.0
     OVERRIDE_FIND_PACKAGE
 )
 
