151b514cc19842614378fc23385730bd781d58c1
Reinhard <567396+suamor@users.noreply.github.com>
2026-07-16 08:43:40 +0200

    ...

--- CMake/LuaJIT.cmake
+++ CMake/LuaJIT.cmake
@@ -33,7 +33,7 @@ ExternalProject_Add(
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/lauxlib.h      <INSTALL_DIR>/include/ &&
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.h       <INSTALL_DIR>/include/ &&
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luaconf.h      <INSTALL_DIR>/include/ &&
-        ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.lib     <INSTALL_DIR>/lib/
+        ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/luajit.lib     <INSTALL_DIR>/lib/ &&
         ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/src/lua51.lib      <INSTALL_DIR>/lib/
 #    COMMAND cmd /c dir /s "${LUAJIT_PREFIX_NATIVE}"
 )
