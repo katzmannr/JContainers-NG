b09e977e5c1c2cbaa1686924561f781c1408002b
Reinhard <567396+suamor@users.noreply.github.com>
2026-07-17 21:54:29 +0200

    Make python run unbuffered to see if install run issues resolve (no output shown) Add global exception in main part of install to get traceback on log

--- tools/install.py
+++ tools/install.py
@@ -114,37 +114,42 @@ def make_archive (dst, src):
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    if len (sys.argv) < 3:
-        print ("Usage: install.py x64/[Release[VR|GOG]|Debug[VR|GOG]] [64|VR|GOG]")
-        exit (1)
+    try:
+        if len (sys.argv) < 3:
+            print ("Usage: install.py x64/[Release[VR|GOG]|Debug[VR|GOG]] [64|VR|GOG]")
+            exit (1)
 
-    JCLib.suffix = sys.argv[2]
-    JCLib.name = "JContainers" + JCLib.suffix + ".dll"
+        JCLib.suffix = sys.argv[2]
+        JCLib.name = "JContainers" + JCLib.suffix + ".dll"
 
-    config = Config (sys.argv[1])
+        config = Config (sys.argv[1])
 
-    print ("Setup Skyrim Mod tree...")
-    srcdata = os.path.join (ROOT, 'src', 'Data')
-    shutil.rmtree (config.dataDir, ignore_errors=True)
-    shutil.copytree (srcdata, config.dataDir)
-    shutil.copy2 (os.path.join (config.origin, JCLib.name), config.jcLibPath)
+        print ("Setup Skyrim Mod tree...")
+        srcdata = os.path.join (ROOT, 'src', 'Data')
+        shutil.rmtree (config.dataDir, ignore_errors=True)
+        shutil.copytree (srcdata, config.dataDir)
+        shutil.copy2 (os.path.join (config.origin, JCLib.name), config.jcLibPath)
 
-    print ("Generate and compile scripts...")
-    config.jcLib.produce_code (config.pscDir)
-    compile_scripts (config.pscDir, config.compiledDir)
+        print ("Generate and compile scripts...")
+        config.jcLib.produce_code (config.pscDir)
+        compile_scripts (config.pscDir, config.compiledDir)
 
-    print ("Recreate distros...")
-    shutil.rmtree (DIST_DIR, ignore_errors = True)
+        print ("Recreate distros...")
+        shutil.rmtree (DIST_DIR, ignore_errors = True)
 
-    dst = os.path.join (DIST_DIR, 'Data')
-    shutil.copytree (config.dataDir, dst, \
-            ignore = shutil.ignore_patterns ('*.exp', '*.lib', 'test_data'))
-    make_archive (os.path.join (DIST_DIR, \
-            'JContainers' + JCLib.suffix + '-v' + config.jcLib.version ()), dst)
-    shutil.rmtree (dst, ignore_errors = True)
+        dst = os.path.join (DIST_DIR, 'Data')
+        shutil.copytree (config.dataDir, dst, \
+                ignore = shutil.ignore_patterns ('*.exp', '*.lib', 'test_data'))
+        make_archive (os.path.join (DIST_DIR, \
+                'JContainers' + JCLib.suffix + '-v' + config.jcLib.version ()), dst)
+        shutil.rmtree (dst, ignore_errors = True)
 
-    print ("API example skipped - its under revision...")
-    print ("All done.")
+        print ("API example skipped - its under revision...")
+        print ("All done.")
+    except Exception:
+            import traceback
+            traceback.print_exc()
+            raise
 
 #------------------------------------------------------------------------------
 
